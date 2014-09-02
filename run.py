77156de7326b43bd1a1a22798f64311b21c95c50
baohongbin [鲍红彬] <baohongbin@xiaomi.com>
2014-09-02 10:32:59 +0800

    remove

--- run.py
+++
@@ -1,9 +0,0 @@
-import nose
-import sys
-# modify loop as what you want.
-loop = 1
-if sys.argv[1] == '--loop':
-    loop = sys.argv[2]
-inputs = ['', '--with-plan-loader','--with-file-output','--verbosity=2']
-for i in range(int(loop)):
-    nose.run(argv=inputs)
